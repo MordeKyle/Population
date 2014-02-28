5770687729a0f5bea7afcbd2b1e36c7e28037794
Kyle McBride <Kyle@MordeKyle.com>
2014-02-27 22:33:24 -0600

    Version: 1.0 Optimistic Robin
    02/27/2014 @ 20:54
    •added everything to form
    numberOfOrganismsTxt and Label
    amountOfIncreaseTxt and Label
    daysToMultiplyTxt and Label
    calculateButton
    resetButton
    exitButton
    outputListBox
    •added pretty much everything code wise
    TryParse on all data
    created while loop with calculations and counter
    created output format
    added statements to reset and exit buttons as well

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.numberOfOrganismsTxt = new System.Windows.Forms.TextBox();
+            this.amountOfIncreaseTxt = new System.Windows.Forms.TextBox();
+            this.daysToMultiplyTxt = new System.Windows.Forms.TextBox();
+            this.outputListBox = new System.Windows.Forms.ListBox();
+            this.numberOfOrganismsLabel = new System.Windows.Forms.Label();
+            this.amountOfIncreaseLabel = new System.Windows.Forms.Label();
+            this.daysToMultipleLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // numberOfOrganismsTxt
+            // 
+            this.numberOfOrganismsTxt.Location = new System.Drawing.Point(153, 47);
+            this.numberOfOrganismsTxt.Name = "numberOfOrganismsTxt";
+            this.numberOfOrganismsTxt.Size = new System.Drawing.Size(100, 20);
+            this.numberOfOrganismsTxt.TabIndex = 0;
+            // 
+            // amountOfIncreaseTxt
+            // 
+            this.amountOfIncreaseTxt.Location = new System.Drawing.Point(153, 94);
+            this.amountOfIncreaseTxt.Name = "amountOfIncreaseTxt";
+            this.amountOfIncreaseTxt.Size = new System.Drawing.Size(100, 20);
+            this.amountOfIncreaseTxt.TabIndex = 1;
+            // 
+            // daysToMultiplyTxt
+            // 
+            this.daysToMultiplyTxt.Location = new System.Drawing.Point(153, 141);
+            this.daysToMultiplyTxt.Name = "daysToMultiplyTxt";
+            this.daysToMultiplyTxt.Size = new System.Drawing.Size(100, 20);
+            this.daysToMultiplyTxt.TabIndex = 2;
+            // 
+            // outputListBox
+            // 
+            this.outputListBox.FormattingEnabled = true;
+            this.outputListBox.Location = new System.Drawing.Point(287, 12);
+            this.outputListBox.Name = "outputListBox";
+            this.outputListBox.Size = new System.Drawing.Size(185, 238);
+            this.outputListBox.TabIndex = 3;
+            this.outputListBox.TabStop = false;
+            // 
+            // numberOfOrganismsLabel
+            // 
+            this.numberOfOrganismsLabel.AutoSize = true;
+            this.numberOfOrganismsLabel.Location = new System.Drawing.Point(12, 50);
+            this.numberOfOrganismsLabel.Name = "numberOfOrganismsLabel";
+            this.numberOfOrganismsLabel.Size = new System.Drawing.Size(107, 13);
+            this.numberOfOrganismsLabel.TabIndex = 4;
+            this.numberOfOrganismsLabel.Text = "Amount of Organisms";
+            // 
+            // amountOfIncreaseLabel
+            // 
+            this.amountOfIncreaseLabel.AutoSize = true;
+            this.amountOfIncreaseLabel.Location = new System.Drawing.Point(12, 97);
+            this.amountOfIncreaseLabel.Name = "amountOfIncreaseLabel";
+            this.amountOfIncreaseLabel.Size = new System.Drawing.Size(131, 13);
+            this.amountOfIncreaseLabel.TabIndex = 5;
+            this.amountOfIncreaseLabel.Text = "Average Daily Increase(%)";
+            // 
+            // daysToMultipleLabel
+            // 
+            this.daysToMultipleLabel.AutoSize = true;
+            this.daysToMultipleLabel.Location = new System.Drawing.Point(12, 144);
+            this.daysToMultipleLabel.Name = "daysToMultipleLabel";
+            this.daysToMultipleLabel.Size = new System.Drawing.Size(81, 13);
+            this.daysToMultipleLabel.TabIndex = 6;
+            this.daysToMultipleLabel.Text = "Days of Inrease";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(16, 227);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(97, 227);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 4;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(178, 227);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 262);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.daysToMultipleLabel);
+            this.Controls.Add(this.amountOfIncreaseLabel);
+            this.Controls.Add(this.numberOfOrganismsLabel);
+            this.Controls.Add(this.outputListBox);
+            this.Controls.Add(this.daysToMultiplyTxt);
+            this.Controls.Add(this.amountOfIncreaseTxt);
+            this.Controls.Add(this.numberOfOrganismsTxt);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox numberOfOrganismsTxt;
+        private System.Windows.Forms.TextBox amountOfIncreaseTxt;
+        private System.Windows.Forms.TextBox daysToMultiplyTxt;
+        private System.Windows.Forms.ListBox outputListBox;
+        private System.Windows.Forms.Label numberOfOrganismsLabel;
+        private System.Windows.Forms.Label amountOfIncreaseLabel;
+        private System.Windows.Forms.Label daysToMultipleLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button exitButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//Program: Population
+//Author: Kyle McBride A02609917
+//Date: 02/27/2014
+//Description: Application that predicts the approximate size of a population of organisms. The application will 
+//             use text boxes to allow the user to enter the starting number of organisms, the average daily 
+//             population increase (as a percentage), and the number of days the organisms will be left to multiply.
+//                                        ***SEE CHANGELOG IN SOLUTION DIRECTORY***
 
 namespace Population
 {
@@ -15,6 +22,76 @@ namespace Population
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //declaring variables for data user inputs
+                decimal startingAmount;
+                decimal rateOfIncrease;
+                decimal daysOfIncrease;
+                int loopCountAmount = 1;
+                decimal currentAmount;
+                //variable for calculations during loop
+                currentAmount = decimal.Parse(numberOfOrganismsTxt.Text);
+                
+                
+                //checking to make sure initial amount of organisms was entered correctly and parsing the data
+                if (decimal.TryParse(numberOfOrganismsTxt.Text, out startingAmount))
+                {
+                    //checking to make sure the rate of organism growth was entered correctly and parsing the data
+                    if (decimal.TryParse(amountOfIncreaseTxt.Text, out rateOfIncrease))
+                    {
+                        //putting this data into the correct format for mathmatical calculations
+                        rateOfIncrease /= 100;
+
+                        //checking to make sure the amount of days the organism will grow is entered correctly and parsing
+                        if (decimal.TryParse(daysToMultiplyTxt.Text, out daysOfIncrease))
+                        {
+                            //loop that displays data and does calculations
+                            while (outputListBox.Items.Count < daysOfIncrease)
+                            {
+                                outputListBox.Items.Add("Day " + loopCountAmount + " = " + currentAmount);
+                                currentAmount = (rateOfIncrease * currentAmount) + currentAmount;
+                                loopCountAmount += 1;
+                            }
+                        }
+                        else //error essage for not entering the amount of days
+                        {
+                            MessageBox.Show("Please enter the amount of days the organisms will grow.");
+                        }
+                    }
+                    else //error message for not entering a percentage
+                    {
+                        MessageBox.Show("Please enter a rate in percentage(without the % character) for organsism growth.");
+                    }
+                }
+                else //error message for not entering amount of organisms
+                {
+                    MessageBox.Show("Please enter how many organisms you are starting with.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            //clearing everything out
+            numberOfOrganismsTxt.Text = "";
+            amountOfIncreaseTxt.Text = "";
+            daysToMultiplyTxt.Text = "";
+            outputListBox.Items.Clear();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //closing the application
+            this.Close();
         }
     }
 }
